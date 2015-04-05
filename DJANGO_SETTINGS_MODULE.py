47ac09d0205ca67bef9ff098c23e10d83e5f9efe
Can Knaut <can_knaut@yahoo.de>
2015-04-05 14:25:30 +0200

    rerun collectstatic on heroku, configured environment variable

---
+++ DJANGO_SETTINGS_MODULE.py
@@ -0,0 +1,2 @@
+export DJANGO_SETTINGS_MODULE=cknautcom.settings
+heroku config:set DJANGO_SETTINGS_MODULE=cknautcom.settings -account can_knaut@yahoo.de
--- cknautcom/settings.py
+++ cknautcom/settings.py
@@ -111,14 +111,9 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
