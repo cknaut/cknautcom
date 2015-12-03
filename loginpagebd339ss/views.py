2ca35d2c44efb82cbf4ee0cac02923152645525a
Can Knaut <can_knaut@yahoo.de>
2015-12-04 00:33:31 +0100

    loginagain

--- loginpagebd339ss/views.py
+++ loginpagebd339ss/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth import login, authenticate, logout
+from django.http import redirect
 
 import os
 pw = os.environ.get('BD339SSPW')
@@ -7,5 +8,8 @@ def login_url(request):
     if request.method == 'POST':
             password = request.POST['password']
             if password==pw:
-                return HttpResponse ("Success")
+                return redirect ("http://31.24.12.49/")
     return render(request, 'landingpage/login.html')   
+
+
+
